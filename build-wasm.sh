cc7d51b84ff29a026ded1b54be72bbbcecf02924
mikan-919 <131849133+mikan-919@users.noreply.github.com>
2026-06-06 17:58:43 +0900

    Reset.

--- build-wasm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Building for wasm32-unknown-unknown..."
-cargo build --release --target wasm32-unknown-unknown
-
-echo "Wasm files copied to iris-web/pkg/"
--- src/analyzer/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod errors;
-pub mod symbol;
-#[cfg(test)]
-mod tests;
-pub mod type_checker;
-
-pub use errors::TypeError;
-pub use symbol::{OwnershipState, Symbol, SymbolTable};
-pub use type_checker::TypeChecker;
